6e4dac08970015685484b433c23981c889a91361
taufik69 <taufik35-2915@diu.edu.bd>
2026-02-18 13:43:50 +0600

    fix: image quality

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{
-      hostname: "images.unsplash.com",
-      protocol: "https",
-    }, {
-      protocol:"https",
-      hostname:"res.cloudinary.com"
-    }],
-
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+        protocol: "https",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+    qualities: [70, 75, 100],
   },
 };
 
