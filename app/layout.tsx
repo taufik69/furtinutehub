72f5d03c90f8a6df4a448e91ab24882724cd9f21
Taufik islam <taufik35-2915@diu.edu.bd>
2026-04-22 09:48:28 +0600

    layout script tag removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 import { GoogleTagManager } from "@next/third-parties/google";
 
 import Footer from "@/components/commonComponents/Footer";
